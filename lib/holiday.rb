68fca276e2d9333d342f39dd5d6717269a21478c
Sung Bae <singsang2@gmail.com>
2018-10-08 04:00:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,23 @@ def all_holidays_with_bbq(holiday_hash)
 end
 
 
+  let(:holiday_supplies) {
+    {
+      :winter => {
+        :christmas => ["Lights", "Wreath"],
+        :new_years => ["Party Hats"]
+      },
+      :summer => {
+        :fourth_of_july => ["Fireworks", "BBQ"]
+      },
+      :fall => {
+        :thanksgiving => ["Turkey"]
+      },
+      :spring => {
+        :memorial_day => ["BBQ"]
+      }
+    }
+  }
 
 
 
